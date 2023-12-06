c47d3df4d7e14efb204379533916df78e7a3b68a
NDoolan360 <n.doolan360@gmail.com>
2023-12-06 17:35:55 +1100

    Day 5 - Pass

--- days/day_05.ts
+++ days/day_05.ts
@@ -1,5 +1,5 @@
 const solve_05 = (_input: string): [string, string] => {
-    
+    // Passed, did it just in rust
 
     return ["".toString(), "".toString()];
 }
@@ -8,5 +8,5 @@ const solve_05 = (_input: string): [string, string] => {
 // cat ./input/2023_day_4_input.txt | bun ./aoc-2023-ts/days/day_04.ts
 process.stdin.setEncoding('utf-8');
 process.stdin.on('data', (input: string) => {
-    console.log(solve_04(input));
+    console.log(solve_05(input));
 });
